3913c2d8396635eaa93c619ca8b5e19fc26bf809
Elvinp329 <elvinkj@code.edu.az>
2023-01-03 00:07:53 +0400

    A2 task1

---
+++ A2 task1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace A2_task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            if ( n>= 1 && n<= 1188 )
+            {
+                int il = n / 12;
+                int ay = n % 12;
+                Console.WriteLine($"{n} ay={il} il ve ay {ay}");
+
+
+            }
+            else
+            {
+                Console.WriteLine("1 - 1188 intervalinda eded daxil edilmelidir ");
+
+            }
+
+
+
+
+
+
+            Console.WriteLine("Hello ");
+        }
+    }
+}
